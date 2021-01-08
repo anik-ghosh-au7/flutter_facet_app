7829c670e212e10ed0abf4eaebafe65761fbe87b
Anik Ghosh <tech.anikghosh@gmail.com>
2021-01-08 21:09:51 +0530

    drawer style changed

--- lib/filters.dart
+++ lib/filters.dart
@@ -72,129 +72,145 @@ class _MyFiltersState extends State<Filters> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(0, 105, 0, 0),
-      child: Column(
-        children: [
-          Container(
-            child: Align(
-              alignment: Alignment.center,
-              child: Container(
-                height: 750,
-                color: Colors.white,
-                child: Scaffold(
-                  appBar: CustomAppBar(
-                    height: 50,
-                    child: Column(
+    return Container(
+      width: 350,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(0, 105, 0, 0),
+        child: Column(
+          children: [
+            Container(
+              child: Align(
+                alignment: Alignment.center,
+                child: Container(
+                  height: 750,
+                  color: Colors.white,
+                  child: Scaffold(
+                    appBar: CustomAppBar(
+                      height: 50,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          RichText(
+                            text: TextSpan(
+                                text: 'Selects Authors',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                          ),
+                        ],
+                      ),
+                    ),
+                    body: loading
+                        ? Center(child: CircularProgressIndicator())
+                        : ListView(
+                            children: aggregationData.keys.map((key) {
+                              return Container(
+                                child: Column(
+                                  children: [
+                                    new CheckboxListTile(
+                                      controlAffinity:
+                                          ListTileControlAffinity.leading,
+                                      activeColor: Colors.black54,
+                                      dense: true,
+                                      title: new Text(key),
+                                      value: aggregationData[key],
+                                      onChanged: (bool value) {
+                                        setState(() {
+                                          aggregationData[key] = value;
+                                        });
+                                        widget.setValue(key);
+                                      },
+                                    ),
+                                    const Divider(
+                                      color: Colors.black,
+                                      height: 10,
+                                      thickness: 0.1,
+                                      indent: 25,
+                                      endIndent: 20,
+                                    )
+                                  ],
+                                ),
+                              );
+                            }).toList(),
+                          ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              child: Align(
+                alignment: Alignment.center,
+                child: Container(
+                  color: Colors.black,
+                  height: 70,
+                  width: 500,
+                  child: Center(
+                    child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        RichText(
-                          text: TextSpan(
-                              text: 'Selects Authors',
+                        Expanded(
+                          flex: 6,
+                          child: RaisedButton(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 10.0, vertical: 23.0),
+                            color: Colors.black,
+                            child: Text(
+                              'Apply',
                               style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                              )),
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white,
+                              ),
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                        Expanded(
+                          flex: 2,
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: RichText(
+                              text: TextSpan(
+                                text: '|',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 50,
+                                    fontWeight: FontWeight.w100),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          flex: 6,
+                          child: RaisedButton(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 10.0, vertical: 23.0),
+                            color: Colors.black,
+                            child: Text(
+                              'Close',
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white,
+                              ),
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            // shape: RoundedRectangleBorder(
+                            //     borderRadius: BorderRadius.circular(50.0)),
+                          ),
                         ),
                       ],
                     ),
                   ),
-                  body: loading
-                      ? Center(child: CircularProgressIndicator())
-                      : ListView(
-                          children: aggregationData.keys.map((key) {
-                            return new CheckboxListTile(
-                              activeColor: Colors.black54,
-                              dense: true,
-                              title: new Text(key),
-                              value: aggregationData[key],
-                              onChanged: (bool value) {
-                                setState(() {
-                                  aggregationData[key] = value;
-                                });
-                                widget.setValue(key);
-                              },
-                            );
-                          }).toList(),
-                        ),
                 ),
               ),
-            ),
-          ),
-          Container(
-            child: Align(
-              alignment: Alignment.center,
-              child: Container(
-                color: Colors.black,
-                height: 70,
-                width: 500,
-                child: Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        flex: 6,
-                        child: RaisedButton(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 10.0, vertical: 23.0),
-                          color: Colors.black,
-                          child: Text(
-                            'Apply',
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.w300,
-                              color: Colors.white,
-                            ),
-                          ),
-                          onPressed: () {},
-                          // shape: RoundedRectangleBorder(
-                          //     borderRadius: BorderRadius.circular(50.0)),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: Align(
-                          alignment: Alignment.center,
-                          child: RichText(
-                            text: TextSpan(
-                              text: '|',
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 50,
-                                  fontWeight: FontWeight.w100),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 6,
-                        child: RaisedButton(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 10.0, vertical: 23.0),
-                          color: Colors.black,
-                          child: Text(
-                            'Close',
-                            style: TextStyle(
-                              fontSize: 20.0,
-                              fontWeight: FontWeight.w300,
-                              color: Colors.white,
-                            ),
-                          ),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          // shape: RoundedRectangleBorder(
-                          //     borderRadius: BorderRadius.circular(50.0)),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
